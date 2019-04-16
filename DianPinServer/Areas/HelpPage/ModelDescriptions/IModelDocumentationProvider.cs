5dab0077b5a4827ea6ad5acaaa5ecf5ce2423ae9
Mavolas <sgngqian@sina.com>
2019-04-16 22:45:13 +0800

    initial

---
+++ DianPinServer/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace DianPinServer.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ DianPinServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DianPinServer.WebApiApplication" Language="C#" %>
